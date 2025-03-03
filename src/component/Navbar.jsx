5af4850b7838ff2c5c6c981d81de015f82403fc6
DeepshikhaJana <deepshikhajana2004@gmail.com>
2025-03-04 01:49:18 +0530

    updated navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 function Navbar() {
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <img src={assets.logo} className="w-40" px-0 alt="" />
@@ -23,6 +24,26 @@ function Navbar() {
           <hr className="w-2/4 border-none h-[1.5px] bg-slate-500 hidden" />
         </NavLink>
       </ul>
+      <div className='flex items-center gap-6'>
+        <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+
+        <div className='group relative'>
+          <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+          <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500'>
+              <p  className='cursor-pointer hover:text-black'>My Profile</p>
+              <p className='cursor-pointer hover:text-black'>Orders</p>
+              <p className='cursor-pointer hover:text-black'>Log Out</p>
+            </div>
+          </div>
+        </div>
+
+        <Link to='/cart' className='relative'>
+        <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
+        <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+        </Link>
+        
+      </div>
     </div>
   );
 }
