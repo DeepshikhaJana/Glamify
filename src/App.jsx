7bf22c687e5482984cb7d82d25cee3ee1111d0ea
DeepshikhaJana <deepshikhajana2004@gmail.com>
2025-03-04 02:20:39 +0530

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,13 @@ import Orders from './pages/Orders';
 import Cart from './pages/Cart';
 import Navbar from './component/Navbar';
 
+
 function App() {
  
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar/>
+      
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/collection" element={<Collection/>} />
