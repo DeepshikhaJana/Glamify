e74f920c15b748d88906c6bbfa39ccb4edf225d7
Deepshikha Jana <129770228+DeepshikhaJana@users.noreply.github.com>
2025-08-02 02:22:24 +0530

    Updated About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Title from '../component/Title'
+import React from 'react';
+import Title from '../component/Title';
 import { assets } from '../assets/assets';
 
 function About() {
